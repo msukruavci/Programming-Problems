6585b2d34829ec1ff40470960b05e6253dadd4ce
msukruavci <130562831+msukruavci@users.noreply.github.com>
2024-05-09 15:33:01 +0300

    Delete Codewars/C/Beginner/Problem1-Pillars directory

--- Codewars/C/Beginner/Problem1-Pillars/Pillars.c
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/5bb0c58f484fcd170700063d
-long pillars(int num_of_pillars, int distance, int width) {
-  long result;
-  if (num_of_pillars < 2) return 0;
-  result =( (num_of_pillars - 1) * (distance * 100) ) + (num_of_pillars - 2) * width;
-  return result;
-}
